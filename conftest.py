45d30a9439f15bfee4cbf771389cc4bd2be53de2
blue-velvet <31938637+blue-velvet@users.noreply.github.com>
2018-08-05 19:35:58 +0300

    fixture bug fixed

--- conftest.py
+++ conftest.py
@@ -4,23 +4,22 @@ from fixture.application import Application
 fixture = None
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def app(request):
     global fixture
     if fixture is None:
         fixture = Application()
-        fixture.session.login()
     else:
         if not fixture.is_valid():
             fixture = Application()
-            fixture.session.login()
+    fixture.session.login()
     return fixture
 
 
-@pytest.fixture#(scope="session", autouse=True)
+@pytest.fixture(scope='session', autouse=True)
 def stop(request):
     def fin():
         fixture.session.logout()
         fixture.destroy()
-    request.addfinalizer(fin())
+    request.addfinalizer(fin)
     return fixture
--- fixture/contact.py
+++ fixture/contact.py
@@ -2,11 +2,14 @@ class ContactHelper:
     def __init__(self, app):
         self.app = app
 
+
     def add(self, contact):
         wd = self.app.wd
+        self.app.open_home_page()
         wd.find_element_by_xpath("//a[@href='edit.php']").click()
         self.fill_form(contact)
         wd.find_element_by_name("submit").click()
+        self.app.open_home_page()
 
     def fill_form(self, contact):
         self.type("firstname", contact.firstname)
@@ -22,13 +25,17 @@ class ContactHelper:
 
     def modify(self, contact):
         wd = self.app.wd
+        self.app.open_home_page()
         wd.find_element_by_css_selector("[src='icons/pencil.png']").click()
         self.type("firstname", contact.firstname)
         self.type("lastname", contact.lastname)
         wd.find_element_by_name("update").click()
+        self.app.open_home_page()
 
     def delete(self):
         wd = self.app.wd
+        self.app.open_home_page()
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         wd.switch_to_alert().accept()
+        self.app.open_home_page()
